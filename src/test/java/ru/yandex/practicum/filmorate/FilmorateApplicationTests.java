52af0c4a8b83555f397968f28106407f2e0b9895
Irina Belikova <beli1rina@yandex.ru>
2025-07-19 02:35:40 +0300

    добавление тестов

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -5,40 +5,59 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.model.User;
+import ru.yandex.practicum.filmorate.storage.film.FilmDbStorage;
+import ru.yandex.practicum.filmorate.storage.friendsStatus.FriendsStatusDbStorage;
+import ru.yandex.practicum.filmorate.storage.friendsStatus.FriendsStatusStorage;
+import ru.yandex.practicum.filmorate.storage.genreType.GenreTypeDbStorage;
+import ru.yandex.practicum.filmorate.storage.mpa.MpaDbStorage;
 import ru.yandex.practicum.filmorate.storage.user.UserDbStorage;
 
 import java.util.Optional;
+import java.util.Set;
 
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.assertj.core.api.Assertions.assertThat;
 
-//@SpringBootTest
-//class FilmorateApplicationTests {
-//
-//    @Test
-//    void contextLoads() {
-//    }
-//}
 @JdbcTest
 @AutoConfigureTestDatabase
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
-@Import({UserDbStorage.class})
+@Import({UserDbStorage.class, FriendsStatusDbStorage.class, FriendsStatusDbStorage.class,
+        FilmDbStorage.class, MpaDbStorage.class, GenreTypeDbStorage.class})
 class FilmorateApplicationTests {
     private final UserDbStorage userStorage;
+    private final FriendsStatusStorage friendsStatusStorage;
+    private final FilmDbStorage filmDbStorage;
 
     @Test
-    public void testFindUserById() {
+    public void testUser() {
 
-//        Optional<User> userOptional = userStorage.getUserById(1);
-//        User userOptional = userStorage.getUserById(1);
-        Optional<User> userOptional = Optional.of(userStorage.getUserById(1));
-
+        Optional<User> userOptional = Optional.ofNullable(userStorage.getUserById(1));
         assertThat(userOptional)
                 .isPresent()
                 .hasValueSatisfying(user ->
-                        assertThat(user).hasFieldOrPropertyWithValue("id", 1)
+                        assertThat(user)
+                                .hasFieldOrPropertyWithValue("id", 1L)
                 );
+
+        Set<Long> friendsIds = friendsStatusStorage.getFriendsId(1);
+        assertThat(friendsIds)
+                .isNotNull()
+                .hasSize(2);
+
+        friendsStatusStorage.deleteAllFriends(1);
+        assertThat(friendsStatusStorage.getFriendsId(1)).isEmpty();
+    }
+
+    @Test
+    public void testFilm() {
+        Optional<Film> filmOptional = Optional.ofNullable(filmDbStorage.getFilmById(1));
+        assertThat(filmOptional)
+                .isPresent()
+                .hasValueSatisfying(film -> {
+                    assertThat(film.getId()).isEqualTo(1L);
+                    assertThat(film.getGenres()).hasSize(3);
+                });
     }
 }
--- src/test/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorageTest.java
+++ src/test/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorageTest.java
@@ -2,12 +2,14 @@ package ru.yandex.practicum.filmorate.storage.film;
 
 import org.junit.jupiter.api.Test;
 import ru.yandex.practicum.filmorate.controller.FilmController;
+import ru.yandex.practicum.filmorate.dto.MpaDto;
 import ru.yandex.practicum.filmorate.exception.ValidationException;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
 import ru.yandex.practicum.filmorate.storage.user.InMemoryUserStorage;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -17,8 +19,14 @@ class InMemoryFilmStorageTest {
 
     @Test
     void shouldBeCreateAndUpdate() {
-        Film film = Film.builder().name("название").description("описание")
-                .releaseDate(LocalDate.of(2022, 1, 1)).duration(100L).build();
+        Film film = Film.builder()
+                .name("название")
+                .description("описание")
+                .releaseDate(LocalDate.of(2022, 1, 1))
+                .duration(100L)
+                .mpa(new MpaDto(1))
+                .genres(new ArrayList<>())
+                .build();
         filmStorage.createFilm(film);
         List<Film> films = filmStorage.getAll();
         assertEquals(1, films.size(), "Фильм не добавился в таблицу.");
